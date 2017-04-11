90bfb3fedd503ee1bba92bd0d35e482ea55b5ba0
Evans R <evansrutoh@gmail.com>
2017-04-11 11:54:27 +0800

    Structure update

---
+++ serverTest/udpServer.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Evans R. on 4/11/2017.
+ */
+
+var PORT = 13370;
+var HOST = '127.0.0.1';
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+server.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+
+});
+
+server.bind(PORT, HOST);
