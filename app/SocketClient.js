aa5a096a773b53be9ca461d1dcc005e47e49e4cb
Evans R <evansrutoh@gmail.com>
2017-04-14 06:39:03 +0800

    SocketClient Class Created

---
+++ app/SocketClient.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Evans R. on 4/14/2017.
+ */
+
+class SocketClient {
+    constructor(){
+
+    }
+
+    udpTransmit(){
+
+    }
+
+    /**
+     * To be implemented in future version.
+     * TODO: Implement TCP Transmit client
+     */
+    tcpTransmit(){
+        console.warn("TCP TRASMIT: Warning - This is just a stub - Operation not implemnted")
+    }
+
+}
